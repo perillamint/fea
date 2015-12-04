ce40c79f763d381077dcf2d9e912c3e2b12a87bc
perillamint <perillamint@gentoo.moe>
2015-12-04 23:55:03 +0900

    Add neuron implementation

--- src/neuron.rs
+++ src/neuron.rs
@@ -5,35 +5,91 @@ pub struct Dendrite {
 }
 
 pub struct Neuron {
-    sources: Vec<Dendrite>,
+    dendrites: Vec<Dendrite>,
     threshold: i32,
     result_new: i32,
     pub result: i32
 }
 
+impl PartialEq for Neuron {
+    fn eq(&self, other: &Neuron) -> bool {
+        self == other
+    }
+
+    fn ne(&self, other: &Neuron) -> bool {
+        !self.eq(other)
+    }
+}
+
 impl Neuron {
     pub fn new(threshold: i32) -> Neuron {
         Neuron {
-            sources: Vec::new(),
+            dendrites: Vec::new(),
             threshold: threshold,
             result_new: 0,
             result: 0
         }
     }
 
-    pub fn add_dendrite(&self, dendrite: Dendrite) {
-        //TODO: Impl add_dendrite
+    fn search_dendrite(&self, neuron: &Neuron) -> Option<usize> {
+        let mut iter = self.dendrites.iter();
+        let mut cnt: usize = 0;
+
+        loop {
+            match iter.next() {
+                Some(dendrite) => {
+                    if dendrite.source == *neuron {
+                        return Some(cnt);
+                    }
+                },
+                None => return None,
+            }
+
+            cnt += 1;
+        }
     }
 
-    pub fn remove_dendrite(&self, dendrite: Dendrite) {
-        //TODO: impl.
+    pub fn add_dendrite(&mut self, dendrite: Dendrite) {
+        let idx = self.search_dendrite(&dendrite.source);
+
+        match idx {
+            Some(_) => panic!("Duplicated dendrites!"),
+            None => self.dendrites.push(dendrite),
+        }
     }
 
-    pub fn do_calculation() {
-        //TODO: Impl do_calculation
+    pub fn remove_dendrite(&mut self, neuron: Neuron) {
+        let idx = self.search_dendrite(&neuron);
+
+        match idx {
+            Some(x) => {
+                self.dendrites.remove(x);
+            },
+            None => return,
+        }
     }
 
-    pub fn expose_result(&self) {
+    pub fn do_calculation(&mut self) {
+        let mut iter = self.dendrites.iter();
+        let mut acc: i32 = 0;
+
+        loop {
+            match iter.next() {
+                Some(dendrite) => {
+                    acc += dendrite.source.result * dendrite.weight;
+                },
+                None => break,
+            }
+        }
+
+        if acc > self.threshold {
+            self.result_new = 1;
+        } else {
+            self.result_new = 0;
+        }
+    }
+
+    pub fn expose_result(&mut self) {
         self.result = self.result_new;
     }
 }
